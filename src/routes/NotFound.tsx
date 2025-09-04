f063029ed48a017f281753feeddd4cbd06905caa
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-04 11:14:14 -0300

    Updated NotFound.tsx

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,12 +1,11 @@
-import { Link, isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
-  const err = useRouteError();
-  const msg = isRouteErrorResponse(err) ? `${err.status} ${err.statusText}` : 'Página no encontrada';
   return (
     <main style={{ padding: 24 }}>
-      <h1>{msg}</h1>
-      <p>Volver al <Link to="/">inicio</Link>.</p>
+      <h1>Página no encontrada</h1>
+      <p>La ruta solicitada no existe.</p>
+      <p><Link to="/">Volver al inicio</Link></p>
     </main>
-  );
+  )
 }
