8d043ed4e486e1056ad854860e4c024f2cbcef12
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-04 11:05:27 -0300

    Updated useModal.ts

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,1 +1,8 @@
-useModal.ts
+import { useCallback, useState } from 'react';
+
+export function useModal(initial = false){
+  const [open, setOpen] = useState(initial);
+  const show = useCallback(() => setOpen(true), []);
+  const hide = useCallback(() => setOpen(false), []);
+  return { open, show, hide };
+}
