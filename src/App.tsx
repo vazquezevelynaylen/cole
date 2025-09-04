d5e4efe92256e5317a26dea8bff2810e26b2b88d
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-04 11:13:47 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { Outlet } from 'react-router-dom';
-import Header from '@components/header/Header';
-import Subnav from '@components/nav/Subnav';
-import Footer from '@components/footer/Footer';
-import A11yControls from '@components/a11y/A11yControls';
+import { Routes, Route } from 'react-router-dom'
+import Home from '@routes/Home'
+import NotFound from '@routes/NotFound'
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <Subnav />
-      <main id="contenido" tabIndex={-1}>
-        <Outlet />
-      </main>
-      <Footer />
-      <A11yControls />
-    </>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
 }
