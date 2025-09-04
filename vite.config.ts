5588990d689fe18d5a2135be6925ee3118bf5909
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-04 10:28:36 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  build: { target: 'esnext' }
-})
+  server: { port: 5173, open: true },
+  preview: { port: 5174 },
+  build: {
+    sourcemap: true,
+    outDir: 'dist'
+  }
+});
