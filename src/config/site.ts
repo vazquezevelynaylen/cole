e23287e6e3a8cc7255108458b2a3d31b433007d9
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-04 11:06:58 -0300

    Updated site.ts

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const site = {
+  name: 'E.T. 20 D.E. 20 "Carolina Muzilli"',
+  address: 'Av. Directorio 6462, Mataderos, CABA',
+  phone: '(011) 1234-5678',
+  email: 'contacto@colegio.edu.ar',
+  socials: {
+    instagram: '#',
+    youtube: '#'
+  }
+};
