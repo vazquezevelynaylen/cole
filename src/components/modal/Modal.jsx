a60c0e45ad93b245cd2d1d0c7bb7f78e9a0fc12e
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:12:50 -0300

    Updated Modal.jsx

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,20 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Modal;
-var react_1 = require("react");
-require("./modal.css");
-function Modal(_a) {
-    var open = _a.open, onClose = _a.onClose, title = _a.title, children = _a.children;
-    (0, react_1.useEffect)(function () {
-        var onKey = function (e) { if (e.key === 'Escape' && open)
-            onClose(); };
-        document.addEventListener('keydown', onKey);
-        return function () { return document.removeEventListener('keydown', onKey); };
-    }, [open, onClose]);
-    if (!open)
-        return null;
-    return (<div className="modal-root" role="dialog" aria-modal="true" aria-labelledby="modal-title">
-      <div className="modal-backdrop" onClick={onClose}/>
+import './modal.css'
+
+export default function Modal({ open, title = 'Aviso', onClose, children }){
+  if(!open) return null
+  return (
+    <div className="modal-root" role="dialog" aria-modal="true" aria-labelledby="modal-title">
+      <div className="modal-backdrop" onClick={onClose} />
       <div className="modal">
         <h3 id="modal-title">{title}</h3>
         <div>{children}</div>
@@ -23,5 +13,6 @@ function Modal(_a) {
           <button type="button" autoFocus onClick={onClose}>Entendido</button>
         </div>
       </div>
-    </div>);
+    </div>
+  )
 }
