52f015166ad7fede57911cbfeacaff4494be2ccf
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:16:47 -0300

    Updated site.js

--- src/config/site.js
+++ src/config/site.js
@@ -1,13 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.site = void 0;
-exports.site = {
-    name: 'E.T. 20 D.E. 20 "Carolina Muzilli"',
-    address: 'Av. Directorio 6462, Mataderos, CABA',
-    phone: '(011) 1234-5678',
-    email: 'contacto@colegio.edu.ar',
-    socials: {
-        instagram: '#',
-        youtube: '#'
-    }
-};
+export const site = {
+  name: 'E.T. 20 D.E. 20 "Carolina Muzilli"',
+  address: 'Av. Directorio 6462, CABA',
+  phone: '+54-11-1234-5678',
+  url: 'https://tusitio.ejemplo/'
+}
