ab05c83dba898d51f2692dc63e05f6c01941b00b
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:18:38 -0300

    Updated useModal.js

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,11 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useModal = useModal;
-var react_1 = require("react");
-function useModal(initial) {
-    if (initial === void 0) { initial = false; }
-    var _a = (0, react_1.useState)(initial), open = _a[0], setOpen = _a[1];
-    var show = (0, react_1.useCallback)(function () { return setOpen(true); }, []);
-    var hide = (0, react_1.useCallback)(function () { return setOpen(false); }, []);
-    return { open: open, show: show, hide: hide };
+import { useState, useCallback } from 'react'
+export function useModal(defaultOpen=false){
+  const [open, setOpen] = useState(defaultOpen)
+  const show = useCallback(()=> setOpen(true),[])
+  const hide = useCallback(()=> setOpen(false),[])
+  return { open, show, hide }
 }
