994be539ee50d313e71413b4aa144e658f23c5fb
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:19:41 -0300

    Updated useIntersection.js

--- src/hooks/useIntersection.js
+++ src/hooks/useIntersection.js
@@ -1,20 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useIntersection = useIntersection;
-var react_1 = require("react");
-function useIntersection(opts) {
-    var ref = (0, react_1.useRef)(null);
-    var _a = (0, react_1.useState)(true), isIntersecting = _a[0], setIntersecting = _a[1];
-    (0, react_1.useEffect)(function () {
-        var el = ref.current;
-        if (!el)
-            return;
-        var io = new IntersectionObserver(function (_a) {
-            var e = _a[0];
-            return setIntersecting(e.isIntersecting);
-        }, opts);
-        io.observe(el);
-        return function () { return io.disconnect(); };
-    }, [opts]);
-    return [ref, isIntersecting];
+import { useEffect } from 'react'
+export function useIntersection(target, options, onChange){
+  useEffect(()=>{
+    if(!target?.current) return
+    const io = new IntersectionObserver((entries)=> onChange(entries[0]), options)
+    io.observe(target.current)
+    return ()=> io.disconnect()
+  }, [target, options, onChange])
 }
