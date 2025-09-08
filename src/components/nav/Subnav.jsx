bbbbeb8a4a2b6a884fd51ada9aedb325b45d4d5b
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 09:36:44 -0300

    Create missing Subnav component

---
+++ src/components/nav/Subnav.jsx
@@ -0,0 +1,7 @@
+export default function Subnav() {
+  return (
+    <nav className="subnav" aria-label="Navegación secundaria">
+      {/* Subnav content can be added here as needed */}
+    </nav>
+  )
+}
