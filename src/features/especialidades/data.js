9424de58562af004f5649df2d6c147f11ba09c42
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 11:14:43 -0300

    Updated data.js

---
+++ src/features/especialidades/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id:'tic',
+    title:'Tecnologías de la Información y la Comunicación (TIC)',
+    text:'Programación, redes, sistemas y prácticas en laboratorio.',
+    img:'/img/aulacompu1.jpg',
+    alt:'Laboratorio de TICs',
+    links:[
+      {label:'Plan TIC', href:'https://…'}
+    ]
+  },
+  {
+    id:'multimedia',
+    title:'Producción Multimedial',
+    text:'Diseño, animación, edición y comunicación digital.',
+    img:'/img/multi1.jpg',
+    alt:'Área Multimedia',
+    links:[
+      {label:'Plan Multimedia', href:'https://…'}
+    ]
+  }
+]
