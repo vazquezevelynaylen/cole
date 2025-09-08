4972322cada6df22d3b580e35e2287689869634f
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:20:25 -0300

    Updated data.js

--- src/features/comunicados/data.js
+++ src/features/comunicados/data.js
@@ -1,8 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DATA = void 0;
-exports.DATA = [
+export const DATA = [
     { fecha: '2025-08-29', titulo: 'Acto por Aniversario', detalle: 'Acto institucional en el SUM. Turnos mañana y tarde.', link: '#', tag: 'importante', estado: 'Vigente' },
     { fecha: '2025-08-27', titulo: 'Inscripción a mesas', detalle: 'Abren inscripciones para mesas de septiembre.', link: '#', tag: 'info', estado: 'Nuevo' },
     { fecha: '2025-08-21', titulo: 'Corte de luz', detalle: 'Reprogramación de actividades por mantenimiento eléctrico.', link: '#', tag: 'urgente', estado: 'Importante' }
-];
+]
