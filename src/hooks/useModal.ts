fb07948e07ee905d2b26eee28c657a61f66f98ea
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:17:58 -0300

    Deleted useModal.ts

--- src/hooks/useModal.ts
+++
@@ -1,8 +0,0 @@
-import { useCallback, useState } from 'react';
-
-export function useModal(initial = false){
-  const [open, setOpen] = useState(initial);
-  const show = useCallback(() => setOpen(true), []);
-  const hide = useCallback(() => setOpen(false), []);
-  return { open, show, hide };
-}
