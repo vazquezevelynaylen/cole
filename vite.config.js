bf5c49db72cef7a3d3452ee579db6e85818129e6
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 09:28:02 -0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@components': path.resolve(__dirname, 'src/components'),
       '@styles': path.resolve(__dirname, 'src/styles'),
-    },
-  },
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@config': path.resolve(__dirname, 'src/config'),
+      '@api': path.resolve(__dirname, 'src/api'),
+    }
+  }
 })
