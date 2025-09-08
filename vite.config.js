2f3d3ce7783bd47cc9d462ee81fecd4b266746d2
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:57:44 -0300

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { resolve } from 'path'
 
-export default defineConfig({
+export default {
   plugins: [react()],
   resolve: {
     alias: {
-      '@routes': path.resolve(__dirname, 'src/routes'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@features': path.resolve(__dirname, 'src/features'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@lib': path.resolve(__dirname, 'src/lib'),
-      '@config': path.resolve(__dirname, 'src/config'),
-      '@api': path.resolve(__dirname, 'src/api'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@features': resolve(__dirname, 'src/features'),
+      '@styles': resolve(__dirname, 'src/styles')
     }
-  }
-})
-  
+  },
+  server: { port: 5173 },
+  preview: { port: 5173 }
+}
