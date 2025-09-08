3097617ab4316b63a0ac9a9199c1736b7d222773
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:54:27 -0300

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import '@styles/globals.css'
 
-const container = document.getElementById('root')
+const rootEl = document.getElementById('root')
 if (!rootEl) throw new Error('No se encontró el elemento #root')
 
 createRoot(rootEl).render(
