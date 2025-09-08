a9f797b72980952eabbaf5692c1dfd1e91d21f04
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-08 10:21:21 -0300

    Updated types.js

--- src/features/comunicados/types.js
+++ src/features/comunicados/types.js
@@ -1,10 +1,1 @@
-export type Tag = 'all' | 'urgente' | 'importante' | 'info'
-
-export type Comunicado = {
-  fecha: string // ISO YYYY-MM-DD
-  titulo: string
-  detalle: string
-  link: string
-  tag: Exclude<Tag, 'all'>
-  estado: string
-}
+export const TAGS = ['urgente', 'imortante', 'info']
