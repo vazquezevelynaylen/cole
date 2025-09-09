6d2e2504a7b52fc99a31c29bdfa47714a727e79f
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-09 10:27:40 -0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import Home from './routes/Home'
-import EspecialidadesPage from './routes/Especialidades'
-import NotFound from './routes/NotFound'
+import Home from './routes/Home.jsx'
+import NotFound from './routes/NotFound.jsx'
 
-export default function App(){
+export default function App() {
   return (
-    <HelmetProvider>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/especialidades" element={<EspecialidadesPage/>} />
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
-    </HelmetProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
