2b9280de0a3af3246cc44241104aaa6ae3d939d8
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-09 10:19:35 -0300

    Updated Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,48 +1,18 @@
-import './header.css'
-
 export default function Header() {
   return (
-    <header>
-      {/* Título a la izquierda */}
-      <h1>E.T. 20 D.E. 20 "Carolina Muzilli"</h1>
-
-      {/* Menú horizontal */}
-      <nav aria-label="Navegación principal">
-        <ul className="menu">
-          <li><a href="#top">Inicio</a></li>
-
-          <li className="has-submenu">
-            <button aria-haspopup="true" aria-expanded="false">Institucional</button>
-            <ul className="submenu" role="menu">
-              <li><a href="#institucional">Presentación</a></li>
-              <li><a href="#institucional">Autoridades</a></li>
-              <li><a href="#institucional">Ubicación &amp; 360°</a></li>
-            </ul>
-          </li>
-
-          <li className="has-submenu">
-            <button aria-haspopup="true" aria-expanded="false">Especialidades</button>
-            <ul className="submenu" role="menu">
-              <li><a href="#especialidades">TICs</a></li>
-              <li><a href="#especialidades">Multimedia</a></li>
-              <li><a href="#academico">Calendario</a></li>
-            </ul>
-          </li>
-
-          <li className="has-submenu">
-            <button aria-haspopup="true" aria-expanded="false">Académico</button>
-            <ul className="submenu" role="menu">
-              <li><a href="#comunicados">Avisos</a></li>
-              <li><a href="#comunicados">Exámenes</a></li>
-              <li><a href="#comunicados">Boletines</a></li>
-            </ul>
-          </li>
-
-          <li><a href="#proyectos">Proyectos</a></li>
-          <li><a href="#novedades">Novedades</a></li>
-          <li><a href="#contacto">Contacto</a></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <div className="header__brand">E.T. 20 D.E. 20 "Carolina Muzilli"</div>
+        <nav className="nav" aria-label="Navegación principal">
+          <ul className="nav__list">
+            <li><a className="nav__link" href="#top">Inicio</a></li>
+            <li><a className="nav__link" href="#especialidades">Especialidades</a></li>
+            <li><a className="nav__link" href="#comunicados">Comunicados</a></li>
+            <li><a className="nav__link" href="#contacto">Contacto</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div className="header-spacer" aria-hidden="true"></div>
+    </>
   )
 }
