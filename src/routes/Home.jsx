ab31e16fa1e6a94e039fe2531136e9e5dd946868
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-09 11:16:47 -0300

    Updated Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,6 +10,17 @@ import Footer from '../components/footer/Footer.jsx'
 import BackToTop from '@components/ui/BackToTop';
 import PlanCard from '@components/planes/PlanCard';
 import useReveal from '@hooks/useReveal';
+import BackToTop from '../components/ui/BackToTop.jsx'
+
+export default function Home() {
+  return (
+    <>
+      {/* …contenido… */}
+      <BackToTop />
+    </>
+  )
+}
+
 
 
 export default function Home() {
