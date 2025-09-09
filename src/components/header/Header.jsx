7493a8ec2ee0d0a1735743d6ca62f6a0fc22d1a4
vazquezevelynaylen <vazquezevelyn.et20@gmail.com>
2025-09-09 10:29:09 -0300

    Updated Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,49 @@
-export default function Header() {
+import './header.css'
+
+export default function Header(){
   return (
     <>
+      <div className="nav-backdrop" hidden></div>
       <header>
-        <div className="header__brand">E.T. 20 D.E. 20 "Carolina Muzilli"</div>
-        <nav className="nav" aria-label="Navegación principal">
-          <ul className="nav__list">
-            <li><a className="nav__link" href="#top">Inicio</a></li>
-            <li><a className="nav__link" href="#especialidades">Especialidades</a></li>
-            <li><a className="nav__link" href="#comunicados">Comunicados</a></li>
-            <li><a className="nav__link" href="#contacto">Contacto</a></li>
+        <h1>E.T. 20 D.E. 20 "Carolina Muzilli"</h1>
+
+        <nav aria-label="Navegación principal" id="primary-menu">
+          <ul className="menu">
+            <li><a href="#top" id="link-inicio">Inicio</a></li>
+
+            <li className="has-submenu">
+              <button aria-haspopup="true" aria-expanded="false">Institucional</button>
+              <ul className="submenu" role="menu">
+                <li><a href="#institucional">Presentación</a></li>
+                <li><a href="#institucional">Autoridades</a></li>
+                <li><a href="#institucional">Ubicación &amp; 360°</a></li>
+              </ul>
+            </li>
+
+            <li className="has-submenu">
+              <button aria-haspopup="true" aria-expanded="false">Especialidades</button>
+              <ul className="submenu" role="menu">
+                <li><a href="#especialidades">TICs</a></li>
+                <li><a href="#especialidades">Multimedia</a></li>
+                <li><a href="#academico">Calendario</a></li>
+              </ul>
+            </li>
+
+            <li className="has-submenu">
+              <button aria-haspopup="true" aria-expanded="false">Académico</button>
+              <ul className="submenu" role="menu">
+                <li><a href="#comunicados">Avisos</a></li>
+                <li><a href="#comunicados">Exámenes</a></li>
+                <li><a href="#comunicados">Boletines</a></li>
+              </ul>
+            </li>
+
+            <li><a href="#proyectos">Proyectos</a></li>
+            <li><a href="#novedades">Novedades</a></li>
+            <li><a href="#contacto">Contacto</a></li>
           </ul>
         </nav>
       </header>
-      <div className="header-spacer" aria-hidden="true"></div>
     </>
   )
 }
