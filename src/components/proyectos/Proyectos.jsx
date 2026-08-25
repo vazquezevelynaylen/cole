1b68acb6fe5470c8e29baadaa63a8dc1b4d5981e
evelyn <vazquezevelyn.et20@gmail.com>
2026-08-25 16:51:40 -0300

    Proyectos: rotacion automatica de imagenes cada 4s con fundido

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -1,17 +1,29 @@
+import { useState, useEffect } from 'react'
 import './Proyectos.css'
 
 const SLIDES = [
   { left: '/img/tic1.jpg', right: '/img/multi1.jpg' },
+  { left: '/img/multi1.jpg', right: '/img/colegio1.jpg' },
+  { left: '/img/colegio1.jpg', right: '/img/tic1.jpg' },
 ]
 
 export default function Proyectos() {
-  const slide = SLIDES[0]
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setIndex((i) => (i + 1) % SLIDES.length)
+    }, 4000)
+    return () => clearInterval(id)
+  }, [])
+
+  const slide = SLIDES[index]
 
   return (
     <section className="proyectos" id="proyectos" aria-labelledby="ttl-proy">
       <div className="proyectos__grid">
         <div className="proyectos__media">
-          <img src={slide.left} alt="Proyecto TIC" className="proyectos__img" />
+          <img key={`l-${index}`} src={slide.left} alt="Proyecto" className="proyectos__img" />
         </div>
 
         <div className="proyectos__center">
@@ -19,7 +31,7 @@ export default function Proyectos() {
         </div>
 
         <div className="proyectos__media">
-          <img src={slide.right} alt="Proyecto Multimedia" className="proyectos__img" />
+          <img key={`r-${index}`} src={slide.right} alt="Proyecto" className="proyectos__img" />
         </div>
       </div>
     </section>
